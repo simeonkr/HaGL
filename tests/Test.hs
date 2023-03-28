179fcc57398a4090d62878aa2fe2aacbf9047069
Simeon Krastnikov <skrastnikov@gmail.com>
2023-03-28 19:42:43 +0100

    Add a few tests and re-organize existing ones

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,30 +1,33 @@
+import Prelude hiding (const, sin, cos, sqrt, length)
+
 import Test.HUnit
 import Control.Monad (when)
 import System.Exit
 import System.Directory (removeFile)
 import qualified Data.ByteString as BS
+import qualified Data.List as List
 import qualified Graphics.UI.GLUT as GLUT
 
 import Graphics.HEGL
 import Graphics.HEGL.Internal (dumpGlsl, hostEval)
 
 
--- Unit test setup
+-- Test setup
 
-data GenericUnitTest d where
-    GenericUnitTest :: String -> GLExpr d Bool -> GenericUnitTest d
+data HEGLTest d where
+    HEGLTest :: String -> GLExpr d Bool -> HEGLTest d
 
 -- verify that 'expr' evaluates to True
-hostUnitTest :: GenericUnitTest HostDomain -> Test
-hostUnitTest (GenericUnitTest label expr) = 
+mkHostTest :: HEGLTest HostDomain -> Test
+mkHostTest (HEGLTest label expr) = 
     TestLabel (label ++ "_host") $ TestCase $ do
         let ioev = error "Tests do not support evaluation of I/O GLExprs"
         res <- hostEval ioev expr
         assertBool ("Failed to evaluate to true on host: " ++ label) res
 
 -- verify that setting color to 'expr' produces a white image
-shaderUnitTest :: GenericUnitTest FragmentDomain -> Test
-shaderUnitTest (GenericUnitTest label expr) = 
+mkShaderTest :: HEGLTest FragmentDomain -> Test
+mkShaderTest (HEGLTest label expr) = 
     TestLabel (label ++ "_shader") $ TestCase $ do
         let quadPos = vert 
                 [(vec2 (-1) (-1)), 
@@ -51,42 +54,200 @@ shaderUnitTest (GenericUnitTest label expr) =
         assertBool ("Failed to obtain a true value in shader: " ++ label) success
 
 
--- Unit tests
+eps = 1e-4 :: GLExpr d Float
+closeEnough x y = abs ((x - y) / y) .< eps
 
-genericUnitTests :: [GenericUnitTest d]
-genericUnitTests = [
-        trivial,
-        booleanExpr1,
-        tmp,
-        vec4Constr
+
+-- Tests
+
+genericTests :: [HEGLTest d]
+genericTests = [
+        trivialTest,
+        vec2Test,
+        vec3Test,
+        vec4Test,
+        arrayTest,
+        --rawMatConstrTest,
+        glLiftTest,
+        booleanExprTest,
+        lengthTest,
+        distanceTest,
+        --dotTest,
+        crossTest,
+        --normalizeTest,
+        faceforwardTest,
+        reflectTest,
+        refractTest,
+        vecArithmeticTest,
+        trigIdentTest
     ]
 
-trivial = GenericUnitTest "trivial" $
-    true
+hostTests :: [HEGLTest HostDomain]
+hostTests = [
 
-booleanExpr1 = GenericUnitTest "boolean_expr1" $
-    false .|| true .|| false .== true
+    ]
 
-tmp = GenericUnitTest "tmp" $
-    Graphics.HEGL.sin (1 :: GLExpr d Float) + Graphics.HEGL.sin 2 .> Graphics.HEGL.sin 1 + (1 + 2)
+shaderTests :: [HEGLTest d]
+shaderTests = [
 
-vec4Constr = GenericUnitTest "vec4_constr" $
+    ]
+
+
+trivialTest = HEGLTest "trivial" true
+
+
+-- Vector and matrix (de-)construction and indexing
+
+vec2Test = HEGLTest "vec2" $
+    let v = vec2 1 2 :: GLExpr d (Vec 2 Int)
+        (decon -> (x, y)) = v
+    in v .== 1 + vec2 0 1 .&&
+       v .== 0 + 1 .* v + 0 .&&
+       v .== vec2 1 0 + 2 .* vec2 0 1 .&&
+       v .== vec2 (x_ v) (y_ v) .&&
+       v .== vec2 x y .&&
+       v .== (yx_ . yx_) v .&&
+       v ./= (yx_ . xy_) v
+
+vec3Test = HEGLTest "vec3" $
+    let v = vec3 1 2 3 :: GLExpr d (Vec 3 Int)
+        (decon -> (x, y, z)) = v
+    in v .== pre 1 (vec2 2 3) .&&
+       v .== app (vec2 1 2) 3 .&&
+       v .== 1 + vec3 0 1 2 .&&
+       v .== 0 + 1.* v + 0 .&&
+       v .== vec3 1 0 0 + 2 .* vec3 0 1 0 + 3 .* vec3 0 0 1 .&&
+       v .== vec3 (x_ v) (y_ v) (z_ v) .&&
+       v .== vec3 x y z .&&
+       v .== pre x (vec2 y z) .&&
+       v .== app (vec2 x y) z .&&
+       v .== (zyx_ . zyx_) v .&&
+       v ./= (zyx_ . xyz_) v
+
+vec4Test = HEGLTest "vec4" $
     let v = vec4 1 2 3 4 :: GLExpr d (Vec 4 Int)
+        (decon -> (x, y, z, w)) = v
     in v .== vec2 1 2 $- vec2 3 4 .&&
        v .== pre 1 (vec3 2 3 4) .&&
-       v .== app (vec3 1 2 3) 4
+       v .== app (vec3 1 2 3) 4 .&&
+       v .== 1 + vec4 0 1 2 3 .&&
+       v .== 0 + 1 .* v + 0 .&&
+       v .== vec4 1 0 0 0 + 2 .* vec4 0 1 0 0 + 
+             3 .* vec4 0 0 1 0 + 4 .* vec4 0 0 0 1 .&&
+       v .== vec4 (x_ v) (y_ v) (z_ v) (w_ v) .&&
+       v .== vec4 x y z w .&&
+       v .== pre x (vec3 y z w) .&&
+       v .== app (vec3 x y z) w .&&
+       v .== pre x ((zyx_ . wzy_) v) .&&
+       v .== app ((xyz_ . xyz_) v) w
 
 
--- TODO: test all of the following
---  boolean expressions
---  vectors and geometric functions
---  matrices and geometric functions
---  bitwise expressions
---  basic math expressions
---  accuracy of numerical computations
---  lifted functions
---  uniform == id
---  examples where caching makes a huge difference
+-- Arrays
+
+arrayTest = HEGLTest "array" $
+    let a = uniform $ array [const (2 * i) | i <- [0..1000]] :: GLExpr d [Int]
+    in a .! 0 .== 0 .&& a .! 10 .== 2 * 10 .&&
+       a .! 999 .== 2 * 999
+
+
+-- Lifts from raw types
+
+rawMatConstrTest = HEGLTest "raw matrix constructors" $
+    let m0 = mat3 (vec3 1 4 7) (vec3 2 5 8) (vec3 3 6 9)
+        m1 = uniform $ glLift0 $ fromList [1, 2, 3, 4, 5, 6, 7, 8, 9] :: GLExpr d (Mat 3 3 Float)
+        m2 = uniform $ glLift0 $ fromMapping (\(i, j) -> fromIntegral $ 3 * i + j + 1) :: GLExpr d (Mat 3 3 Float)
+    in m0 .== m1 .&& m0 .== m2 .&& m1 .== m2
+
+glLiftTest = HEGLTest "glLift" $
+    let x = glLift0 [1,2,3,4] :: GLExpr HostDomain [Int]
+        rx = glLift0 [4,3,2,1] :: GLExpr HostDomain [Int]
+        ax = glLift0 [1,2,3,4,4,3,2,1] :: GLExpr HostDomain [Int]
+        f1 = glLift1 List.reverse
+        f2 = glLift2 (List.++)
+    in uniform (f1 x) .== uniform rx .&&
+       uniform (f2 x rx) .== uniform ax
+
+
+-- Type conversion
+
+
+
+-- Boolean and bitwise expressions
+
+booleanExprTest = HEGLTest "boolean expression" $
+    true .|| false .|| true .&& false .|| false .== true
+
+
+-- Common math functions
+
+
+
+-- Geometric functions
+
+lengthTest = HEGLTest "length" $
+    length (vec3 2 (-3) 6 :: GLExpr d (Vec 3 Float)) .== 7 .&&
+    let v@(decon -> (x, y, z, w)) = vec4 1 2 3 4 :: GLExpr d (Vec 4 Float)
+    in length v .== sqrt (x * x + y * y + z * z + w * w)
+
+distanceTest = HEGLTest "distance" $
+    let x = vec4 1 2 3 4 :: GLExpr d (Vec 4 Float)
+        y = vec4 5 6 7 8 :: GLExpr d (Vec 4 Float)
+    in distance x y .== length (x - y)
+
+dotTest = HEGLTest "dot" $
+    let theta = 0.1234 :: GLExpr d Float
+        x = vec2 (cos theta) (sin theta) :: GLExpr d (Vec 2 Float)
+        y = vec2 (cos $ theta + (pi / 2)) (sin $ theta + (pi / 2))
+        z = vec4 1 2 3 4 :: GLExpr d (Vec 4 Float)
+    in closeEnough (dot x y) 0 .&&
+       closeEnough (dot z z) (length z * length z)
+
+crossTest = HEGLTest "cross" $
+    true
+
+normalizeTest = HEGLTest "normalize" $
+    let x = vec4 1 2 3 4 :: GLExpr d (Vec 4 Float)
+    in normalize x .== (1 / length x) .* x
+
+faceforwardTest = HEGLTest "faceforward" $
+    true
+
+reflectTest = HEGLTest "reflect" $
+    true
+
+refractTest = HEGLTest "refact" $
+    true
+
+
+-- Matrix functions
+
+
+
+-- Vector relational functions
+
+
+
+-- Custom function support via glFunc
+
+
+
+-- uniform, prec, & builtin I/O variables
+
+
+
+-- Shader-specific tests: vert, frag
+
+
+
+-- Other miscellaneous tests
+
+vecArithmeticTest = HEGLTest "vector arithmetic" $
+    2 .* vec4 1 2 3 4 - 3 * vec4 1 2 3 4 .== - (vec4 1 2 3 4 :: GLExpr d (Vec 4 Int)) .&&
+    1 .* abs (vec4 1 1 1 1) - abs (-1) .== (vec4 0 0 0 0 :: GLExpr d (Vec 4 Int))
+
+trigIdentTest = HEGLTest "trigonometric identities" $
+    let x0 = 0.1234 :: GLExpr d Float
+    in closeEnough (pow (sin x0) 2 + pow (cos x0) 2) 1
 
 
 runTests :: Test -> IO ()
@@ -96,5 +257,7 @@ runTests tests = do
 
 main :: IO ()
 main = do
-    --runTests $ TestList $ map hostUnitTest genericUnitTests
-    runTests $ TestList $ map shaderUnitTest genericUnitTests
+    runTests $ TestList $ map mkHostTest genericTests
+    runTests $ TestList $ map mkHostTest hostTests
+    runTests $ TestList $ map mkShaderTest genericTests
+    runTests $ TestList $ map mkShaderTest shaderTests
